249dd8c4b70bc53b29e6d20b28da87d7ae005e5e
Jordi Blasco <jordi.blasco@gmail.com>
2015-04-23 16:28:41 +1200

    added logging functionality and loop for all the VMs to be backup

--- bin/modules-backup.sh
+++ bin/modules-backup.sh
@@ -6,11 +6,15 @@
 # For more information, visit the official website : www.hpcnow.com/snow
 #
 
+# If you don't want to backup all the modules, change the following line with a list
+MODULES2BKP=$(snow list| egrep -v "Name|Domain-0"|gawk '{print $1}')
+
 SNOWPATH=$(dirname "$0")
 # Load the configuration
 CONFIG_FILE=$SNOWPATH/../etc/snow.conf
 SNOW_MODULES=$SNOWPATH/../etc/active-modules.conf
 SELF_ACTIVE_MODULES=$(cat $SNOW_MODULES | grep -v ^# | cut -d= -f1 | gawk '{print $1}')
+#MODULES2BKP=${SELF_ACTIVE_MODULES:-$MODULES2BKP}
 
 if [[ -f $CONFIG_FILE ]]; then
     source $CONFIG_FILE
@@ -24,17 +28,20 @@ if [[ -d ! $SNOWROOT/log ]]; then
     mkdir $SNOWROOT/log
 fi
 
-VMNAME=vm.$1
-DATE=$(date +%Y%m%d)
-LAST=$(tail -1 $SNOWROOT/log/snapshots-$VMNAME.history)
+f [[ -d ! $SNOWROOT/backup ]]; then
+    mkdir $SNOWROOT/backup
+fi
 
-/sbin/lvcreate -s -L 1G -n $VMNAME-snap-$DATE.root $SNOWVG/${VMNAME}_root &> $SNOWROOT/log/snapshots-$VMNAME.log
-/sbin/lvcreate -s -L 1G -n $VMNAME-snap-$DATE.var $SNOWVG/${VMNAME}_var &> $SNOWROOT/log/snapshots-$VMNAME.log
-echo "$VMNAME-snap-$DATE" >> $SNOWROOT/log/snapshots-$VMNAME.history
-dd if=/dev/$SNOWVG/$VMNAME-snap-$DATE.root of=$SNOWROOT/backup/$VMNAME-snap-$DATE.root
-dd if=/dev/$SNOWVG/$VMNAME-snap-$DATE.var of=$SNOWROOT/backup/$VMNAME-snap-$DATE.var
-bzip2 $SNOWROOT/backup/$VMNAME-snap-$DATE.*
-rm $SNOWROOT/backup/$LAST.*.bz2 
-/sbin/lvremove -f /dev/$SNOWVG/$LAST.root
-/sbin/lvremove -f /dev/$SNOWVG/$LAST.var
-echo "Backup de $VMNAME realitzat" | nail -s "[sNow!] Snapshot $VMNAME" -a $SNOWROOT/log/snapshots-$VMNAME.log -r $SMAIL $SMAIL
+for VMNAME in $MODULES2BKP 
+do 
+    DATE=$(date +%Y%m%d)
+    LAST=$(tail -1 $SNOWROOT/log/snapshots-$VMNAME.history)
+    /sbin/lvcreate -s -L 1G -n $VMNAME-snap-$DATE $SNOWVG/${VMNAME} &> $SNOWROOT/log/snapshots-$VMNAME.log
+    echo "$VMNAME-snap-$DATE" >> $SNOWROOT/log/snapshots-$VMNAME.history
+    dd if=/dev/$SNOWVG/$VMNAME-snap-$DATE of=$SNOWROOT/backup/$VMNAME-snap-$DATE
+    bzip2 $SNOWROOT/backup/$VMNAME-snap-$DATE
+    rm $SNOWROOT/backup/$LAST.bz2 
+    /sbin/lvremove -f /dev/$SNOWVG/$LAST
+    #echo "Backup module $VMNAME finished" | nail -s "[sNow!] Snapshot $VMNAME" -a $SNOWROOT/log/snapshots-$VMNAME.log -r $SMAIL $SMAIL
+    echo "Backup module $VMNAME finished" | logger -t snow -p user.notice
+done
