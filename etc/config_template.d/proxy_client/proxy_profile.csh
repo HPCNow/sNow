cfcc7be515d9fd4bc065bdf1809938f595639ed6
Jordi Blasco <jordi.blasco@gmail.com>
2016-07-22 04:39:03 +0200

    FIX issues related with slurm client and proxy client

--- etc/config_template.d/proxy_client/proxy_profile.csh
+++ etc/config_template.d/proxy_client/proxy_profile.csh
@@ -1,4 +1,4 @@
 #!/bin/csh
-setent https_proxy __PROXY_SERVER__:__PROXY_PORT__
-setent http_proxy __PROXY_SERVER__:__PROXY_PORT__
-setent ftp_proxy __PROXY_SERVER__:__PROXY_PORT__
+setent https_proxy http://__PROXY_SERVER__:__PROXY_PORT__
+setent http_proxy http://__PROXY_SERVER__:__PROXY_PORT__
+setent ftp_proxy http://__PROXY_SERVER__:__PROXY_PORT__
--- etc/config_template.d/proxy_client/proxy_profile.sh
+++ etc/config_template.d/proxy_client/proxy_profile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-export https_proxy=__PROXY_SERVER__:__PROXY_PORT__
-export http_proxy=__PROXY_SERVER__:__PROXY_PORT__
-export ftp_proxy=__PROXY_SERVER__:__PROXY_PORT__
+export https_proxy=http://__PROXY_SERVER__:__PROXY_PORT__
+export http_proxy=http://__PROXY_SERVER__:__PROXY_PORT__
+export ftp_proxy=http://__PROXY_SERVER__:__PROXY_PORT__
