ff217c41e468ce02ece41064aacd4dc8650953b4
Jordi Blasco <jordi.blasco@gmail.com>
2016-10-02 15:22:01 +1300

    extended source for users root and snow

--- bin/snow-source.sh
+++ bin/snow-source.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-
 SNOWROOT=/sNow
+if [[ $(id -u) -ne 0 || "$USER" == "snow" ]] ; then
+    export PATH=$SNOWROOT/snow-tools/sbin:$PATH
+fi
 export PATH=$SNOWROOT/snow-tools/bin:$PATH
-export LC_ALL=C 
+#export LC_ALL=C 
