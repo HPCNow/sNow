b44fd3a4230c56b6564254dfeaea64a38e95bd6c
Jordi Blasco <jordi.blasco@gmail.com>
2016-10-10 07:38:09 +1300

    moved slurm user scripts management in the login role

--- bin/slurm-source.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-export SBATCH_EXPORT=NONE
-alias sq='squeue -o "%.18i %.9P %.5Q %.8j %.8u %.8T %.10M %.11l %.6D %.4C %.6b %.20S %.20R %.8q"'
-alias squ='squeue -o "%.18i %.9P %.5Q %.8j %.8u %.8T %.10M %.11l %.6D %.4C %.6b %.20S %.20R %.8q" -u $USER'
