e8284275caecfe372010f2e6446028cb17877001
Jordi Blasco <jordi.blasco@gmail.com>
2016-12-14 17:52:49 +1300

    FIX : removed image features from the help - moved to experimental feature

--- share/common.sh
+++ share/common.sh
@@ -103,14 +103,12 @@ function shelp()
         * remove domain <domain>                    | removes an existing domain deployed with sNow!
         * remove node <node>                        | removes an existing node from sNow! configuration
         * remove template <template>                | removes an existing template
-        * remove image <image>                      | removes an existing image
         * list domains                              | list the current domains (services) and their status
         * list templates                            | list the available templates
-        * list images                               | list the available images
         * list nodes                                | list the available compute nodes and their status
-        * boot <domain|server> <image>              | boot specific domain or server with optional image
+        * boot <domain|server>                      | boot specific domain or server
         * boot domains                              | boot all the domains (all services not available under sNow! HA)
-        * boot cluster <cluster> <image>            | boot all the compute nodes of the selected cluster (by default 20 nodes at once)
+        * boot cluster <cluster>                    | boot all the compute nodes of the selected cluster (by default 20 nodes at once)
         * reboot <domain|server>                    | reboot specific domain or server
         * shutdown <domain|server>                  | shutdown specific domain or server
         * shutdown cluster <cluster>                | shutdown all the compute nodes of the selected cluster
@@ -128,6 +126,10 @@ function shelp()
     " 1>&3
 }
 #        * clone <server> <image>            | creates a PXE image to boot the compute nodes diskless
+#        * remove image <image>                      | removes an existing image
+#        * list images                               | list the available images
+#        * boot <domain|server> <image>              | boot specific domain or server with optional image
+#        * boot cluster <cluster> <image>            | boot all the compute nodes of the selected cluster (by default 20 nodes at once)
 
 function end_msg()
 {
