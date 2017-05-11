b3736a0aadf5bfe108b3ac2ecd6265cfb6fdc9bf
Jordi Blasco <jordi.blasco@gmail.com>
2017-05-11 23:14:19 +0200

    fix init script and clusters gmond

--- share/common.d/monitoring.sh
+++ share/common.d/monitoring.sh
@@ -5,6 +5,7 @@
 #
 function setup_ganglia_client()
 {
+    set -x
     case $OS in
         debian|ubuntu)
             pkgs="ganglia-monitor"
@@ -20,13 +21,15 @@ function setup_ganglia_client()
         ;;
     esac
     install_software "$pkgs"
-    if [[ ! -e $SNOW_CONF/system_files/etc/ganglia/gmond.conf ]]; then 
-        chroot ${prefix} /usr/sbin/gmond -t > $SNOW_CONF/system_files/etc/ganglia/gmond.conf
-        sed -i 's|name = "unspecified"|name = "sNow!"|g' $SNOW_CONF/system_files/etc/ganglia/gmond.conf
+    cluster=$(jq ".compute.${HOSTNAME}.cluster" ${SNOW_TOOL}/etc/nodes.json |sed -e 's|"||g')
+    if [[ ! -e $SNOW_CONF/system_files/etc/ganglia/gmond.${cluster}.conf ]]; then 
+        /usr/sbin/gmond -t > $SNOW_CONF/system_files/etc/ganglia/gmond.conf
+        sed -i 's|name = "unspecified"|name = "sNow"|g' $SNOW_CONF/system_files/etc/ganglia/gmond.conf
     fi
-    cp -p $SNOW_CONF/system_files/etc/ganglia/gmond.conf /etc/ganglia/gmond.conf
+    cp -p $SNOW_CONF/system_files/etc/ganglia/gmond_${cluster}.conf /etc/ganglia/gmond.conf
     chown root:root /etc/ganglia/gmond.conf
     chmod 640 /etc/ganglia/gmond.conf
     systemctl enable gmond.service
     systemctl start gmond.service
+    set +x
 } 1>>$LOGFILE 2>&1
