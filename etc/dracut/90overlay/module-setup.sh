a28bda0d7181dfdc56b7086bf98cfb9b66e87218
Jordi Blasco <jordi.blasco@gmail.com>
2017-05-27 01:33:27 +0200

    Included /etc/resolv.conf to avoid potential issues related with DNS service not available while mounting NFS or cluster file systems in the boot time

--- etc/dracut/90overlay/module-setup.sh
+++ etc/dracut/90overlay/module-setup.sh
@@ -11,7 +11,6 @@ check()
 # called by dracut
 depends()
 {
-    #echo network url-lib img-lib nfs
     echo network img-lib nfs dm rootfs-block
     return 0
 }
@@ -24,14 +23,11 @@ installkernel()
 # called by dracut
 install()
 {
+    inst /etc/resolv.conf
     inst_multiple umount curl dmsetup blkid dd losetup grep blockdev find tar gzip bzip2 xz bash
     # make this conditional on a cmdline flag / config option
     inst_multiple -o checkisomd5
     inst_hook cmdline 90 "$moddir/parse-overlay-opts.sh"
-    #inst_dir /overlaymnt
-    #if dracut_module_included "systemd-initrd"; then
-    #    inst_script "$moddir/overlay-generator.sh" $systemdutildir/system-generators/dracut-overlay-generator
-    #fi
     inst_hook pre-pivot 90 "$moddir/overlayroot.sh"
     # requires by SuSE?
     dracut_need_initqueue
