74561eb4c700d4a9411adfbc8ee86d7231e7c44d
Jordi Blasco <jordi.blasco@gmail.com>
2017-10-05 07:09:59 +0200

    Debian Jessie preseed ready for testing

--- etc/config_template.d/boot/templates/debian-8.8-x86_64-default/20-beegfs-client-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dpkg -i /sNow/common/beegfs/deb/*.deb
-cp -p /sNow/common/beegfs/cfg/* /etc/beegfs
-/usr/sbin/beegfs-setup-rdma
-systemctl enable beegfs-helperd.service
-systemctl enable beegfs-client.service
-
